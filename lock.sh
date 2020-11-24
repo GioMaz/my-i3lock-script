d1c2b6829de43c7193ebf5ccc7a7931c41441f89
Giovanni Maria Zanchetta <48056684+GioMaz@users.noreply.github.com>
2020-11-24 10:38:24 +0100

    Add files via upload

---
+++ lock.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+# TAKE A SCREENSHOT
+SCREENSHOT=/tmp/screen.png
+scrot $SCREENSHOT
+
+# GET PREVALENT COLOR (HEX)
+function primcolor {
+	INFO=$(convert $1 -scale 50x50! -depth 8 +dither -colors 8 -format "%c" histogram:info: | \
+		sed -n 's/^[ ]*\(.*\):.*[#]\([0-9a-fA-F]*\) .*$/\1,#\2/p' | sort -r -n -k 1 -t "," | head -n 1 | cut -d "," -f2)
+	echo $INFO
+}
+
+HEX=$(primcolor $SCREENSHOT)
+HEX="${HEX//#}"
+
+# CONVERT TO RGB
+RGB=$(printf "%d %d %d\n" 0x${HEX:0:2} 0x${HEX:2:2} 0x${HEX:4:2})
+R=$(echo $RGB | awk '{print $1;}')
+G=$(echo $RGB | awk '{print $2;}')
+B=$(echo $RGB | awk '{print $3;}')
+SR=$(echo "$R*0.2126" | bc)
+SG=$(echo "$R*0.7152" | bc)
+SB=$(echo "$R*0.0722" | bc)
+LUMA=$(echo "$SR+$SG+$SG" | bc)
+LUMA=${LUMA%.*}
+
+# USE DARK OR LIGHT TEXT
+echo "$LUMA"
+if [ "$LUMA" -lt 396 ]
+then
+  echo "BIANCO"
+  convert $SCREENSHOT \
+  	-filter Gaussian -resize 50% -define filter:sigma=5 -resize 200% \
+  	-font /usr/share/fonts/OpenSans/OpenSans-Light.ttf \
+  	-gravity Center \
+  	-pointsize 40 \
+  	-fill white \
+  	-annotate 0 'Ciao, Giovanni' \
+  	$SCREENSHOT
+else
+  echo "NERO"
+  convert $SCREENSHOT \
+  	-filter Gaussian -resize 50% -define filter:sigma=5 -resize 200% \
+  	-font /usr/share/fonts/OpenSans/OpenSans-Light.ttf \
+  	-gravity Center \
+  	-pointsize 40 \
+  	-fill black \
+  	-annotate 0 'Ciao, Giovanni' \
+  	$SCREENSHOT
+fi
+i3lock -u --ignore-empty-password --show-failed-attempts --nofork --image $SCREENSHOT
+rm $SCREENSHOT
+
+
+#  TEXT SHADOW
+#  convert $SCREENSHOT \
+#  	-font /usr/share/fonts/OpenSans/OpenSans-Light.ttf \
+#  	-gravity Center \
+#  	-pointsize 40 \
+#  	-fill "#333333" \
+#  	-annotate +0+0 'Ciao, Giovanni' \
+#  	-filter Gaussian -resize 50% -define filter:sigma=5 -resize 200% \
+#  	-fill white \
+#  	-annotate 0 'Ciao, Giovanni' \
+#  	$SCREENSHOT
